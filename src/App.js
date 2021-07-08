bccdd6c50df0068c18b81a585c1ff893e55e6775
Ulugbek <ulugbekxurramov20053008@gmail.com>
2021-07-08 19:30:04 +0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState , useEffect } from 'react'
+import './index.css'
 function App() {
+  const[text, setText] = useState("")
+  const [timeRemain, setTimeRemain ]=useState(0)
+  const [isTimeRunning, setTimeRunning] = useState(false)
+  const [words, setWords]= useState(0)
+
+  function handlerChange(e) {
+    setText(e.target.value)
+    // console.log(text);
+  }
+
+  function calcWords(text) {
+    const wordsArray = text.trim().split(" ")
+    const filteredData = wordsArray.filter(word => word !== "");
+    return filteredData.length 
+  }
+   
+  useEffect( ()=> {
+    if (isTimeRunning==true && timeRemain > 0) {
+      setTimeout(() => {
+        setTimeRemain(timeRemain => timeRemain-1 )
+      }, 1000 );  
+    }else if (timeRemain == 0) {
+      setTimeRunning(false)
+      const wordsNumber = calcWords(text)
+      setWords(wordsNumber)
+    }
+    
+  }, [timeRemain, isTimeRunning, ])
+  function startClock() {
+    setTimeRunning(true)
+    setTimeRemain(20)
+    setText("")
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 id="h3">Tezligingizni sinang</h3>
+      <textarea disabled={!isTimeRunning} onChange={handlerChange} value={text}/>
+      <h3 id="h3-two">QOLGAN ⏱: {timeRemain}</h3>
+      <button disabled={isTimeRunning} onClick={ startClock }>Start</button>
+      <h1>So'zlar soni: {words}</h1>
     </div>
   );
 }
 
-export default App;
+export default App
